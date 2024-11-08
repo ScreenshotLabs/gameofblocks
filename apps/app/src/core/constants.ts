9cd1050d6ac18bd0b4a303aaaea5ee16622b42e6
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2024-11-08 15:55:14 +0700

    feat: attack transaction (#15)

--- apps/app/src/core/constants.ts
+++ apps/app/src/core/constants.ts
@@ -1,5 +1,5 @@
 export const CONTRACT_ADDRESS =
-  "0x7e68bfb1f2845d37676521f3c2722e2b5f8c1690218149ae061d8c3703413e8";
+  "0x07e68bfb1f2845d37676521f3c2722e2b5f8c1690218149ae061d8c3703413e8";
 export const RPC_URL = "https://starknet-sepolia.public.blastapi.io";
 export const ARGENT_ACCOUNT_CLASSHASH =
   "0x029927c8af6bccf3f6fda035981e765a7bdbf18a2dc0d630494f8758aa908e2b";
