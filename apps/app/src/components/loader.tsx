10698d8cdcbe16fa27170719b2e35c7920af0cd6
remiroyc <remiroycourt@gmail.com>
2024-11-09 02:34:19 +0700

    feat(app): loader

--- apps/app/src/components/loader.tsx
+++ apps/app/src/components/loader.tsx
@@ -1,7 +1,12 @@
 export default function Loader() {
   return (
-    <div className="absolute inset-0 flex items-center justify-center text-white">
-      Loading game...
-    </div>
+    <div
+      className="absolute inset-0 flex items-center justify-center bg-cover bg-center text-white"
+      style={{
+        backgroundImage: "url('/images/splash-screen@3x.png')",
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+      }}
+    />
   );
 }
