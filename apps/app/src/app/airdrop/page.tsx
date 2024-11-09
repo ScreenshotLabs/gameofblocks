79816b90dfeb5adf3c5516d99d5d8246440c1211
paul launay <launay.p@gmail.com>
2024-11-09 18:48:17 +0700

    feat(airdrop): add airdrop page

--- apps/app/src/app/airdrop/page.tsx
+++ apps/app/src/app/airdrop/page.tsx
@@ -1,31 +1,24 @@
-import Link from "next/link";
+import AirdropButtons from "@/components/airdrop/airdrop-buttons";
+import AirdropImage from "@/components/airdrop/airdrop-image";
 import GameFooter from "@/components/game-footer";
 import GameHeader from "@/components/game-header";
 import { Page } from "@/components/page";
 import { PageTitle } from "@/components/page-title";
-import PremiumBuy from "@/components/premium/premium-buy";
-import PremiumImage from "@/components/premium/premium-image";
 
 export default function AirdropPage() {
   return (
     <Page back>
       <GameHeader />
-      <PageTitle>Premium</PageTitle>
-
+      <PageTitle>Airdrop</PageTitle>
       <div className="my-6 text-center leading-5">
-        Buying a Premium account makes you eligible
+        Create a wallet or connect an existing one
         <br />
-        for our upcoming{" "}
-        <Link href="/airdrop" className="text-[#FFBA08]">
-          airdrop
-        </Link>
-        !
+        to claim your airdrop!
       </div>
-
-      <PremiumImage className="mx-auto" />
-
-      <PremiumBuy />
-
+      <AirdropImage />
+      <div className="flex flex-col items-center">
+        <AirdropButtons />
+      </div>
       <GameFooter />
     </Page>
   );
